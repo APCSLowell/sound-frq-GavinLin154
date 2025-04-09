3c5cc16df0c755eab7f148e84826a0c0fbc656ee
GavinLin154 <84930551+GavinLin154@users.noreply.github.com>
2025-04-08 22:13:10 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -17,16 +17,16 @@ public class Sound
   {  
     /* to be implemented in part (a) */
     int count = 0;
-    for(int i = 0; i < samples.length; i++){
-      if(samples[i]>limit){
+    for(int i = 0; i < samples.length; i++) {
+      if(samples[i] > limit) {
         samples[i] = limit;
         count++;
-      }
-      else if(samples[i]<(-limit)){
-        samples[i] = -limit;
+      } 
+      if(samples[i] < (-1*limit)) {
+        samples[i] = (-1*limit);
         count++;
-      }
-    }
+      }  
+    }  
     return count;
   }
 
@@ -40,18 +40,19 @@ public class Sound
   public void trimSilenceFromBeginning()
   {
     /* to be implemented in part (b) */
-    int count = 0;
-    for(int i = 0; i < samples.length; i++){
-      if(samples[i] == 0){
-        count++;
-      }
-      else{
+    int [] tempList;
+    int numLeadingZero = 0;
+    for(int i = 0; i < samples.length; i++) {
+      if(samples[i] == 0) {
+        numLeadingZero++;
+      } else {
         break;
-      }
+      }  
+    }  
+    tempList = new int[samples.length - numLeadingZero];
+    for(int i = 0; i < tempList.length; i++) {
+      tempList[i] = samples[i + numLeadingZero];
     }
-    int [] newSamples = new int[count];
-      for(int j = count; j > 0; j--){
-        samples.remove(j);
-      }
-  }
+    samples = tempList;
+  } 
 }
